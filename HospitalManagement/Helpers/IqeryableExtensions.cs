414df6b894982a56844f763e585aeec92544bc96
pruthivirajcdac <pruthvi.cdac@gmail.com>
2025-11-27 22:49:13 +0530

    chnages

---
+++ HospitalManagement/Helpers/IqeryableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagement.Helpers
+{
+    public static class IqeryableExtensions
+    {
+        public  static async Task<PagedResult<T>>   ToPagedResult<T>(IQueryable<T> query ,int page, int pageSize)
+        {
+            var totalCount =  await query.CountAsync();
+
+            var items = await  query.Skip((page-1)* pageSize).Take(page).ToListAsync();
+
+            return new PagedResult<T>
+            {
+                TotalCount = totalCount,
+                Items = items
+            };
+        }
+
+    }
+}
